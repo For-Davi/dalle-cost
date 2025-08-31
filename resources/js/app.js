cf2d07dc5001e547719fb0084203afe48320c758
Carlos Davi <carlosdavi004@gmail.com>
2025-08-31 20:33:47 -0300

    Fix: Corrigido app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,7 +6,6 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
     resolve: (name) => resolvePageComponent(`./pages/${name}.vue`, import.meta.glob('./pages/**/*.vue')),
     setup({ el, App, props, plugin }) {
         return createApp({ render: () => h(App, props) })
