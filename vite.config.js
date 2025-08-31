5d717747441f3c626d21e28a1221e99d7007ec09
Carlos Davi <carlosdavi004@gmail.com>
2025-08-31 20:45:36 -0300

    Feat: Instalado alguns componentes do Shadcn

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
 export default defineConfig({
     plugins: [
@@ -19,9 +20,10 @@ export default defineConfig({
         }),
         tailwindcss(),
     ],
-     resolve: {
+    resolve: {
         alias: {
-            '@': '/resources/js',
+            '@': path.resolve(__dirname, './resources/js'),
+            '@/lib': path.resolve(__dirname, './resources/js/lib')
         },
     },
 });
