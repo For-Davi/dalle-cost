b79e9c6fe38b58fcb1e586c2e242ea4f3ee5af4c
Carlos Davi <carlosdavi004@gmail.com>
2025-08-31 22:50:04 -0300

    Feat: Configurado alguns modais

--- resources/js/components/form/FormAuth.vue
+++ resources/js/components/form/FormAuth.vue
@@ -10,6 +10,10 @@
   } from '@/components/ui/card'
   import { Input } from '@/components/ui/input'
   import { Label } from '@/components/ui/label'
+
+  defineOptions({
+    name: 'FormAuth',
+  })
 </script>
 
 <template>
--- resources/js/pages/Data.vue
+++ resources/js/pages/Data.vue
@@ -1,5 +1,9 @@
 <script setup>
   import PanelLayout from '@/layout/PanelLayout.vue'
+  import Button from '@/components/ui/button/Button.vue'
+  import FormIntegral from '@/components/form/FormIntegral.vue'
+  import FormRegister from '@/components/form/FormRegister.vue'
+  import FormOrigin from '@/components/form/FormOrigin.vue'
 
   defineOptions({
     name: 'Data',
@@ -10,9 +14,12 @@
   <PanelLayout>
     <div class="p-6">
       <h1 class="text-2xl font-bold mb-6">Dados</h1>
-      <div class="bg-white p-6 rounded-lg shadow">
-        <p>Aqui você pode gerenciar todos os dados do sistema.</p>
-      </div>
+      <section class="bg-white p-4 rounded-lg shadow flex justify-end gap-2">
+        <FormOrigin />
+        <FormIntegral />
+        <FormRegister />
+      </section>
+      <section>table</section>
     </div>
   </PanelLayout>
 </template>
