5726ef9facd6b7f2beb8365e819df0691c9774a1
Carlos Davi <carlosdavi004@gmail.com>
2025-09-01 00:17:27 -0300

    Feat: Criado seeder para start de usuario

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Carlos Davi',
+            'email' => 'carlosdavi004@gmail.com',
+            'password' => Hash::make('dalle154089'),
+            'role' => 'admin',
+        ]);
+    }
+}
